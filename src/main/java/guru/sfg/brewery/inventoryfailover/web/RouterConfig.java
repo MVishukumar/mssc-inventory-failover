97a838fea0d0bf757aa21fc078db99752f5b0ece
vishukumarm <beingvishu@gmail.com>
2021-11-07 22:30:47 +0530

    Updating to return list of inventoryDto objects.

---
+++ src/main/java/guru/sfg/brewery/inventoryfailover/web/RouterConfig.java
@@ -0,0 +1,22 @@
+package guru.sfg.brewery.inventoryfailover.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class RouterConfig {
+
+    @Bean
+    public RouterFunction inventoryRoute(InventoryHandler inventoryHandler) {
+
+        return route(GET("/inventory-failover")
+                .and(accept(MediaType.APPLICATION_JSON)),inventoryHandler::listInventory);
+    }
+}
